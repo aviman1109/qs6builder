5f24b6208f8feed5e9be4cdc5fb4511afa1537e1
Casper.Cheng <casper.cheng@qbiai.com>
2018-12-09 09:01:10 +0800

    try

--- bin/local/304-bill/build-linux.sh
+++ bin/local/304-bill/build-linux.sh
@@ -7,13 +7,13 @@ echo ${PLUGIN_MODULE} ${PLUGIN_VERSION} ${PLUGIN_BASE}
 
 if [ ! -z "${PLUGIN_MODULE}" ]; then
     echo ${PLUGIN_MODULE} 
-    sed -i 's/value="PLUGIN_MODULE"/value="'"${PLUGIN_MODULE}"'"</g' /tmp/qs-build/bin/local/304-bill/settings.xml
+    sed -i 's/PLUGIN_MODULE/'"${PLUGIN_MODULE}"'</g' /tmp/qs-build/bin/local/304-bill/settings.xml
     echo "set PLUGIN_MODULE complete"
 else
    echo you need settings:PLUGIN_MODULE
 fi
 if [ ! -z "${PLUGIN_VERSION}" ]; then
-    sed -i 's/value="PLUGIN_VERSION"/value="'"${PLUGIN_VERSION}"'"</g' /tmp/qs-build/bin/local/304-bill/settings.xml
+    sed -i 's/PLUGIN_VERSION/'"${PLUGIN_VERSION}"'</g' /tmp/qs-build/bin/local/304-bill/settings.xml
     echo "set PLUGIN_VERSION complete"
 else
    echo you need settings:PLUGIN_VERSION
