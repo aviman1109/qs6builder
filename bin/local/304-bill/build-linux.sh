4526e508255c2b6ee45983714ab0afd6c335098c
Casper.Cheng <casper.cheng@qbiai.com>
2018-12-18 16:50:36 +0800

    fix sed

--- bin/local/304-bill/build-linux.sh
+++ bin/local/304-bill/build-linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo setting...
 count=0
 echo $count
@@ -6,32 +6,28 @@ echo $count
 
 if [ ! -z "${PLUGIN_MODULE}" ]; then
     echo ${PLUGIN_MODULE} 
-    sed -i 's/PLUGIN_MODULE/'"${PLUGIN_MODULE}"'' settings.xml
-    echo "set PLUGIN_MODULE complete"
+    sed -i 's/PLUGIN_MODULE/'"${PLUGIN_MODULE}"'/' settings.xml && echo "set PLUGIN_MODULE complete" || echo "Failure set PLUGIN_MODULE"
     count=`expr $count + 1`
 else
    echo you need settings:PLUGIN_MODULE
 fi
 
 if [ ! -z "${PLUGIN_VERSION}" ]; then
-    sed -i 's/PLUGIN_VERSION/'"${PLUGIN_VERSION}"'' settings.xml
-    echo "set PLUGIN_VERSION complete"
+    sed -i 's/PLUGIN_VERSION/'"${PLUGIN_VERSION}"'/' settings.xml && echo "set PLUGIN_VERSION complete" || echo "Failure set PLUGIN_VERSION"
     count=`expr $count + 1`
 else
    echo you need settings:PLUGIN_VERSION
 fi
 
 if [ ! -z "${PLUGIN_BASE}" ]; then
-    sed -i 's/PLUGIN_BASE/'"${PLUGIN_BASE}"'' settings.xml
-    echo "set PLUGIN_BASE complete"
+    sed -i 's/PLUGIN_BASE/'"${PLUGIN_BASE}"'/' settings.xml && echo "set PLUGIN_BASE complete" || echo "Failure set PLUGIN_BASE"
     count=`expr $count + 1`
 else
    echo you need settings:PLUGIN_BASE
 fi
 
 if [ ! -z "${PLUGIN_INITIALISM}" ]; then
-    sed -i 's/PLUGIN_INITIALISM/'"${PLUGIN_INITIALISM}"'' settings.xml
-    echo "set PLUGIN_INITIALISM complete"
+    sed -i 's/PLUGIN_INITIALISM/'"${PLUGIN_INITIALISM}"'' settings.xml && echo "set PLUGIN_INITIALISM complete" || echo "Failure set PLUGIN_INITIALISM"
     count=`expr $count + 1`
 else
    echo you need settings:PLUGIN_INITIALISM
@@ -41,17 +37,13 @@ fi
 if [ ! -z "${PLUGIN_PORT}" ]; then
     echo ${PLUGIN_PORT}
     x=${PLUGIN_PORT}
-    sed -i 's/PLUGIN_PORT_1/'"${x}"'' settings.xml
-    echo "set httpPort complete"
+    sed -i 's/PLUGIN_PORT_1/'"${x}"'/' settings.xml && echo "set httpPort complete" || echo "Failure set httpPort"
     x=`expr ${x} + 1`
-    sed -i 's/PLUGIN_PORT_2/'"${x}"'' settings.xml
-    echo "set httpsPort complete"
+    sed -i 's/PLUGIN_PORT_2/'"${x}"'/' settings.xml && echo "set httpsPort complete" || echo "Failure set httpsPort"
     x=`expr ${x} + 1`
-    sed -i 's/PLUGIN_PORT_3/'"${x}"'' settings.xml
-    echo "set ajpPort complete"
+    sed -i 's/PLUGIN_PORT_3/'"${x}"'/' settings.xml && echo "set ajpPort complete" || echo "Failure set ajpPort"
     x=`expr ${x} + 1`
-    sed -i 's/PLUGIN_PORT_4/'"${x}"'' settings.xml
-    echo "set shutdownPort complete"
+    sed -i 's/PLUGIN_PORT_4/'"${x}"'/' settings.xml && echo "set ajpPort complete" || echo "Failure set ajpPort"
     count=`expr $count + 1`
 else
    echo you need settings:PLUGIN_PORT
