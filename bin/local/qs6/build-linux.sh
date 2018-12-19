60738570fbc042c843093bff4ede875112e45564
Casper.Cheng <casper.cheng@qbiai.com>
2018-12-19 11:22:48 +0800

    try build

--- bin/local/qs6/build-linux.sh
+++ bin/local/qs6/build-linux.sh
@@ -1,69 +1,43 @@
 #!/bin/bash
+echo
+echo      ┏┛ ┻━━━━━┛ ┻┓
+echo      ┃　　　　　　 ┃
+echo      ┃　　　━　　　┃
+echo      ┃　┳┛　  ┗┳　┃
+echo      ┃　　　　　　 ┃
+echo      ┃　　　┻　　　┃
+echo      ┃　　　　　　 ┃
+echo      ┗━┓　　　┏━━━┛
+echo        ┃　　　┃   神獸保佑
+echo        ┃　　　┃   代碼永無BUG！
+echo        ┃　　　┗━━━━━━━━━┓
+echo        ┃　　　　　　　    ┣┓
+echo        ┃　　　　         ┏┛
+echo        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+echo          ┃ ┫ ┫   ┃ ┫ ┫
+echo          ┗━┻━┛   ┗━┻━┛
+
+
 echo setting...
 count=0
-echo $count
 
 ./setParameter.sh PLUGIN_MODULE ${PLUGIN_MODULE} && count=`expr $count + 1` || echo "count=${count} not add"
-echo $count
 ./setParameter.sh PLUGIN_VERSION ${PLUGIN_VERSION} && count=`expr $count + 1` || echo "count=${count} not add"
-echo $count
 ./setParameter.sh PLUGIN_BASE ${PLUGIN_BASE} && count=`expr $count + 1` || echo "count=${count} not add"
-echo $count
 ./setParameter.sh PLUGIN_INITIALISM ${PLUGIN_INITIALISM} && count=`expr $count + 1` || echo "count=${count} not add"
-echo $count
 
 x=${PLUGIN_PORT}
 for i in {1..4};
 do
-    echo $i
+    echo port:$i
     ./setParameter.sh PLUGIN_PORT_$i ${x} && count=`expr $count + 1` || echo "count=${count} not add"
     x=`expr ${x} + 1`
-    echo $count
 done
 
 
-# ./setParameter.sh PLUGIN_INITIALISM ${PLUGIN_INITIALISM} && count=`expr $count + 1` || echo "count=${count} not add"
-# echo $count
-# if [ ! -z "${PLUGIN_VERSION}" ]; then
-#     sed -i 's/PLUGIN_VERSION/'"${PLUGIN_VERSION}"'/' settings.xml && echo "set PLUGIN_VERSION complete" || echo "Failure set PLUGIN_VERSION"
-#     count=`expr $count + 1`
-# else
-#    echo you need settings:PLUGIN_VERSION
-# fi
-
-# if [ ! -z "${PLUGIN_BASE}" ]; then
-#     sed -i 's/PLUGIN_BASE/'"${PLUGIN_BASE}"'/' settings.xml && echo "set PLUGIN_BASE complete" || echo "Failure set PLUGIN_BASE"
-#     count=`expr $count + 1`
-# else
-#    echo you need settings:PLUGIN_BASE
-# fi
-
-# if [ ! -z "${PLUGIN_INITIALISM}" ]; then
-#     sed -i 's/PLUGIN_INITIALISM/'"${PLUGIN_INITIALISM}"'/' settings.xml && echo "set PLUGIN_INITIALISM complete" || echo "Failure set PLUGIN_INITIALISM"
-#     count=`expr $count + 1`
-# else
-#    echo you need settings:PLUGIN_INITIALISM
-# fi
-
-
-# if [ ! -z "${PLUGIN_PORT}" ]; then
-#     echo ${PLUGIN_PORT}
-#     x=${PLUGIN_PORT}
-#     sed -i 's/PLUGIN_PORT_1/'"${x}"'/' settings.xml && echo "set httpPort complete" || echo "Failure set httpPort"
-#     x=`expr ${x} + 1`
-#     sed -i 's/PLUGIN_PORT_2/'"${x}"'/' settings.xml && echo "set httpsPort complete" || echo "Failure set httpsPort"
-#     x=`expr ${x} + 1`
-#     sed -i 's/PLUGIN_PORT_3/'"${x}"'/' settings.xml && echo "set ajpPort complete" || echo "Failure set ajpPort"
-#     x=`expr ${x} + 1`
-#     sed -i 's/PLUGIN_PORT_4/'"${x}"'/' settings.xml && echo "set shutdownPort complete" || echo "Failure set shutdownPort"
-#     count=`expr $count + 1`
-# else
-#    echo you need settings:PLUGIN_PORT
-# fi
-
-cat settings.xml
 
 if [ $count = 8 ]; then
+    echo Building....
     ../../ant/bin/ant -buildfile build.xml -logfile build.log -Dos=linux -Dmode=full
 else
    echo check your .drone.yml
--- bin/local/qs6/setParameter.sh
+++ bin/local/qs6/setParameter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -z "${2}" ]; then
-    echo ${2} 
+    echo setting....${2} 
     sed -i 's/'"${1}"'/'"${2}"'/g' settings.xml && echo "set ${1} complete" || echo "Failure set ${1}"
 else
    echo you need settings:${1}
