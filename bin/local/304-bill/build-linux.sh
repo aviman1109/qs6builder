d05f0f7e898f8080d0c34f31d034a1a6dbc74e79
Casper.Cheng <casper.cheng@qbiai.com>
2018-12-19 17:29:44 +0800

    try

--- bin/local/304-bill/build-linux.sh
+++ bin/local/304-bill/build-linux.sh
@@ -1,49 +1,4 @@
 #!/bin/bash
-
-echo setting...
-count=0
-
-./setParameter.sh PLUGIN_MODULE ${PLUGIN_MODULE} && count=`expr $count + 1` || echo "count=${count} not add"
-./setParameter.sh PLUGIN_VERSION ${PLUGIN_VERSION} && count=`expr $count + 1` || echo "count=${count} not add"
-./setParameter.sh PLUGIN_BASE ${PLUGIN_BASE} && count=`expr $count + 1` || echo "count=${count} not add"
-./setParameter.sh PLUGIN_INITIALISM ${PLUGIN_INITIALISM} && count=`expr $count + 1` || echo "count=${count} not add"
-
-x=${PLUGIN_PORT}
-for i in {1..4};
-do
-    echo port:$i
-    ./setParameter.sh PLUGIN_PORT_$i ${x} && count=`expr $count + 1` || echo "count=${count} not add"
-    x=`expr ${x} + 1`
-done
-
-
-echo ========end=setting=====================
-
-if [ $count = 8 ]; then
-    echo Building....
-    ../../ant/bin/ant -buildfile build.xml -logfile build.log -Dos=linux -Dmode=full
-    echo ===========end=build==================
-else
-   echo check your .drone.yml
-fi
-
-echo your output file
-cd /tmp/qs-build/output/
-ls *linux*.zip
-
-echo ==============================
-# chmod 777 -R /tmp/qs-build/output/
-# ls /tmp/qs-build/output/
-file=*linux*.zip
-# echo $file
-filename=$file
-if [ -f $filename ]; then
-    cp ${file} /${PLUGIN_BASE}/ && Success || Failure
-else
-   Failure
-   echo "There is nothing in output..."
-fi
-
 Success(){
 
     echo ░░░████████░░███████░░███████░░███████░░
@@ -70,3 +25,47 @@ Failure(){
     echo ░░░░░░░░░░░░░░░░░░
 　
 }
+echo setting...
+count=0
+
+./setParameter.sh PLUGIN_MODULE ${PLUGIN_MODULE} && count=`expr $count + 1` || echo "count=${count} not add"
+./setParameter.sh PLUGIN_VERSION ${PLUGIN_VERSION} && count=`expr $count + 1` || echo "count=${count} not add"
+./setParameter.sh PLUGIN_BASE ${PLUGIN_BASE} && count=`expr $count + 1` || echo "count=${count} not add"
+./setParameter.sh PLUGIN_INITIALISM ${PLUGIN_INITIALISM} && count=`expr $count + 1` || echo "count=${count} not add"
+
+x=${PLUGIN_PORT}
+for i in {1..4};
+do
+    echo port:$i
+    ./setParameter.sh PLUGIN_PORT_$i ${x} && count=`expr $count + 1` || echo "count=${count} not add"
+    x=`expr ${x} + 1`
+done
+
+
+echo ==========end=setting===================
+
+if [ $count = 8 ]; then
+    echo Building....
+    ../../ant/bin/ant -buildfile build.xml -logfile build.log -Dos=linux -Dmode=full
+    echo ===========end=build==================
+else
+   echo check your .drone.yml
+fi
+
+echo your output file
+cd /tmp/qs-build/output/
+ls *linux*.zip
+
+echo ==============================
+# chmod 777 -R /tmp/qs-build/output/
+# ls /tmp/qs-build/output/
+file=*linux*.zip
+# echo $file
+filename=$file
+if [ -f $filename ]; then
+    cp ${file} /${PLUGIN_BASE}/ && Success || Failure
+else
+   Failure
+   echo "There is nothing in output..."
+fi
+
