8135493c4b3cabc88118780090aa5e8e652b9cc8
Casper.Cheng <casper.cheng@qbiai.com>
2018-12-20 23:10:14 +0800

    try

--- quicksilver/linux/killpid.sh
+++ quicksilver/linux/killpid.sh
@@ -4,7 +4,7 @@ echo "ssh pid $1"
 read -a PID <<< "${PIDS}"
 for ((i=0; i < ${#PID[@]}; i++))
 do
-    if [ ! ${PID[$i]} = $$ -a ${PID[$i]} = $1 ]; then
+    if [ ! ${PID[$i]} = $$ -a ! ${PID[$i]} = $1 ]; then
         echo $i ${PID[$i]}
         ppid=${PID[$i]}
         # echo $ppid
